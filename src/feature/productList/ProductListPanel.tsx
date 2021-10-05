2049241709fda8c041ab6c90825b885dcdec2f94
ibrahimcavdar <cavdarhibrahim@gmail.com>
2021-10-05 21:35:07 +0300

    Reformat code

--- src/feature/productList/ProductListPanel.tsx
+++ src/feature/productList/ProductListPanel.tsx
@@ -2,10 +2,10 @@ import { Grid } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useAppSelector } from "../../app/hooks";
 
-type ItemType = 'mug' | 'shirt' | '';
+type ItemType = "mug" | "shirt" | "";
 
-interface Product {
-  tags: string[];
+export interface Product {
+    tags: string[];
     price: number;
     name: string;
     description: string;
@@ -16,28 +16,30 @@ interface Product {
 }
 
 export const ProductListPanel = () => {
-  const [productList, setProductList] = useState<Product[]>([]);
+    const [productList, setProductList] = useState<Product[]>([]);
 
-  const itemType = useAppSelector(state => state.filter);
-  useEffect(() => {
-    fetch(`http://localhost:8080/items?itemType_like=${itemType}&_page=1&_limit=16&_sort=price&_order=asc&tags_like=Person&tags_like=Trees&manufacturer=Leannon-Fahey-and-Sawayn&manufacturer=OHara-Group`)
-    .then((res) => res.json())
-    .then(data=>{
-      setProductList(data);
-      console.log(data);
-    });
-  }, [itemType]);
-  return (
-    <Grid
-      container
-      spacing={{ xs: 2, md: 3 }}
-      columns={{ xs: 4, sm: 8, md: 12 }}
-    >
-      {productList.map((product, index) => (
-        <Grid item xs={2} sm={4} md={4} key={index}>
-          <div>{product.name}</div>
+    const itemType = useAppSelector((state) => state.filter);
+    useEffect(() => {
+        fetch(
+            `http://localhost:8080/items?itemType_like=${itemType}&_page=1&_limit=16&_sort=price&_order=asc&tags_like=Person&tags_like=Trees&manufacturer=Leannon-Fahey-and-Sawayn&manufacturer=OHara-Group`
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                setProductList(data);
+                console.log(data);
+            });
+    }, [itemType]);
+    return (
+        <Grid
+            container
+            spacing={{ xs: 2, md: 3 }}
+            columns={{ xs: 4, sm: 8, md: 12 }}
+        >
+            {productList.map((product, index) => (
+                <Grid item xs={2} sm={4} md={4} key={index}>
+                    <div>{product.name}</div>
+                </Grid>
+            ))}
         </Grid>
-      ))}
-    </Grid>
-  );
+    );
 };
