9bb7711f869bac907a6fc83ad3746ebc9f23edd8
ibrahimcavdar <cavdarhibrahim@gmail.com>
2021-10-09 23:54:16 +0300

    Implement Basket entry with grid

--- src/feature/basket/BasketPanel.tsx
+++ src/feature/basket/BasketPanel.tsx
@@ -1,7 +1,9 @@
 import { Add, Remove } from "@mui/icons-material";
 import {
-    IconButton, List,
-    ListItem, ListItemText
+  Box,
+  Grid,
+  IconButton, List,
+  ListItem, ListItemText, Stack, styled
 } from "@mui/material";
 import React from "react";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
@@ -13,16 +15,17 @@ export const BasketPanel = () => {
 
   const dispatch = useAppDispatch();
 
-  const handleRemove = (product: Product)=>{
+  const handleRemove = (product: Product) => {
     dispatch(removeItemFromBasket(product));
   }
 
-  const handleAdd = (product: Product)=>{
+  const handleAdd = (product: Product) => {
     dispatch(addItemToBasket(product));
   }
 
   return (
     <div>
+
       <List
         sx={{
           width: "100%",
@@ -34,29 +37,36 @@ export const BasketPanel = () => {
         }}
       >
         {Object.keys(basketItems).map((key, index) => {
-          console.log(key,basketItems[key]);
+          console.log(key, basketItems[key]);
           const basketEntry = basketItems[key];
-          const {product,count} = basketEntry;
+          const { product, count } = basketEntry;
           const labelId = `checkbox-list-label-${key}`;
 
           return (
             <ListItem
               key={key}
               disablePadding
-              secondaryAction={
-                <div>
-                  <IconButton edge="end" aria-label="comments">
-                    <Remove onClick={()=>handleRemove(product)} />
-                  </IconButton>
-                  &nbsp;&nbsp;
-                  <span>{count}</span>
-                  <IconButton edge="end" aria-label="comments">
-                    <Add onClick={()=>handleAdd(product)}/>
-                  </IconButton>
-                </div>
-              }
             >
-              <ListItemText id={labelId} primary={`${product.name}`} />
+
+              <Grid container spacing={2}>
+                <Grid item xs container direction="column" spacing={2}  >
+                  <Grid item xs textOverflow={"ellipsis"}>
+                    <div>Product 1</div>
+                    <div>14.99</div>
+                  </Grid>
+                </Grid>
+                <Grid item>
+                  <Stack direction="row" spacing={1}>
+                    <IconButton edge="end">
+                      <Remove onClick={() => handleRemove(product)} />
+                    </IconButton>
+                    <StyledCountBox>{count}</StyledCountBox>
+                    <IconButton edge="end">
+                      <Add onClick={() => handleAdd(product)} />
+                    </IconButton>
+                  </Stack>
+                </Grid>
+              </Grid>
             </ListItem>
           );
         })}
@@ -64,3 +74,10 @@ export const BasketPanel = () => {
     </div>
   );
 };
+
+const StyledCountBox = styled(Box)({
+  textAlign: "center",
+  alignItems: "center",
+  backgroundColor: "#1EA4CE",
+  padding: "10px"
+})
